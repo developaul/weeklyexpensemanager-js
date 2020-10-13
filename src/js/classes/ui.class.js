b7d135f0912d0e117356ecf9aae2bbd81cea14a4
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-13 13:22:45 -0500

    showAlert added, show an alert depending on the type

--- src/js/classes/ui.class.js
+++ src/js/classes/ui.class.js
@@ -1,4 +1,4 @@
-import { total, restante } from '../componentes.js';
+import { total, restante, form } from '../componentes.js';
 
 export class UI {
 
@@ -8,4 +8,18 @@ export class UI {
         total.textContent     = budget;
     }
     
+    showAlert( message, type ) {
+        const divMessage = document.createElement( 'div' );
+        divMessage.textContent = message;
+
+        divMessage.classList.add( 'text-center', 'alert' );
+        divMessage.classList.add( ( type === 'error' ) ? 'alert-danger' : 'alert-success' );
+
+        document.querySelector( '.primario' ).insertBefore( divMessage, form );
+
+        setTimeout( () => {
+            divMessage.remove();
+        }, 3000 );
+    }
+
 }
