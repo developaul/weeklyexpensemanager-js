818ad0d1cdc9bae375aef25e7a853b69bbf77781
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-13 12:43:37 -0500

    insertBudget added, insert the budget and the remaining in the HTML

--- src/js/classes/ui.class.js
+++ src/js/classes/ui.class.js
@@ -1,3 +1,11 @@
+import { total, restante } from '../componentes.js';
+
 export class UI {
+
+    insertBudget( quantity ) {
+        const { budget, remaining } = quantity;
+        restante.textContent  = remaining;
+        total.textContent     = budget;
+    }
     
 }
